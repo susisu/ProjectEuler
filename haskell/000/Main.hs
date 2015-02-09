89e509ee1c47534be56483a03ee144711c363f56
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 20:49:00 +0900

    Add (Haskell, 000)

---
+++ haskell/000/Main.hs
@@ -0,0 +1,13 @@
+{-
+    ProjectEuler 000
+    Haskell / Main.hs
+    copyright (c) 2015 Susisu
+
+    ghc / version 7.8.3
+    `ghc Main.hs -o bin/main`
+-}
+
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, world!"

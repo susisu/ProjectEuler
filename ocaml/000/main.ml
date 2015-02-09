be80029916d181147401afabcd8f57c23bd27b71
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 21:59:32 +0900

    Add (OCaml, 000)

---
+++ ocaml/000/main.ml
@@ -0,0 +1,13 @@
+(*
+    ProjectEuler 000
+    OCaml / main.ml
+    copyright (c) 2015 Susisu
+
+    ocaml / version 4.02.1
+    `ocamlc main.ml -o bin/main`
+*)
+
+
+let main () = print_endline "Hello, world!";;
+
+let _ = main ()

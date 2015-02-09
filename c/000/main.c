0cb547e98b3dd4ec80351d104a3778a455be8679
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 20:39:16 +0900

    Add (C, 000)

---
+++ c/000/main.c
@@ -0,0 +1,15 @@
+/*
+ * ProjectEuler 000
+ * C / main.c
+ * copyright (c) 2015 Susisu
+ *
+ * gcc / Apple LLVM version 6.0 (clang-600.0.56) (based on LLVM 3.5svn)
+ * `gcc main.c -o bin/main`
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    return 0;
+}

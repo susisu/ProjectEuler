86eab418415500ec1ed3ab7fec8269fe4abdb9f1
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 21:15:43 +0900

    Add (Python, 000)

---
+++ python/main.py
@@ -0,0 +1,13 @@
+'''
+    ProjectEuler 000
+    Python / main.py
+    copyright (c) 2015 Susisu
+
+    python / 3.4.2
+    `python3 main.py`
+'''
+
+def main():
+    print("Hello, world!")
+
+main()

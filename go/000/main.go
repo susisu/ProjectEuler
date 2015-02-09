d6b34f1e14e977c47de28f650804f9a8c1bffb4c
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 21:15:34 +0900

    Add (Go, 000)

---
+++ go/000/main.go
@@ -0,0 +1,17 @@
+/*
+ * ProjectEuler 000
+ * Go / main.go
+ * copyright (c) 2015 Susisu
+ *
+ * go / 1.4.1
+ * `go build -o bin/main main.go`
+ */
+
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("Hello, world!\n")
+}

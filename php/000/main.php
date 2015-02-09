9bab0ebe3c50044d40471d210c761e75fa38edae
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 20:52:32 +0900

    Add (PHP, 000)

---
+++ php/000/main.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * ProjectEuler 000
+ * PHP / main.php
+ * copyright (c) 2015 Susisu
+ *
+ * php / 5.4.30
+ * `php main.php`
+ */
+
+function main() {
+    echo "Hello, world!\n";
+}
+
+main();

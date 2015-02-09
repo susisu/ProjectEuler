6a1214f2a131f5298942deb4f9510add6cf63b4f
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 22:05:43 +0900

    Add (Ruby, 000)

---
+++ ruby/000/main.rb
@@ -0,0 +1,14 @@
+=begin
+    ProjectEuler 000
+    Ruby / main.rb
+    copyright (c) 2015 Susisu
+
+    ruby / 2.0.0p481
+    `ruby main.rb`
+=end
+
+def main
+    puts "Hello, world!"
+end
+
+main()

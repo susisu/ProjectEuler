578b2b840c0853945495a82bcb52265a0fe6da5c
Susisu <susisu2413@yahoo.co.jp>
2015-02-09 20:39:23 +0900

    Add (C++, 000)

---
+++ cpp/000/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * ProjectEuler 000
+ * C++ / main.cpp
+ * copyright (c) 2015 Susisu
+ *
+ * g++ / Apple LLVM version 6.0 (clang-600.0.56) (based on LLVM 3.5svn)
+ * `g++ main.cpp -o bin/main`
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}

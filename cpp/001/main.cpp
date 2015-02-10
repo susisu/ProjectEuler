19e607d1aedc058104cfe3671f63b3c4def2dfd1
Susisu <susisu2413@yahoo.co.jp>
2015-02-10 12:59:27 +0900

    Add (C++, 001)

---
+++ cpp/001/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * ProjectEuler 001
+ * C++ / main.cpp
+ * copyright (c) 2015 Susisu
+ *
+ * GCC / Apple LLVM version 6.0 (clang-600.0.56) (based on LLVM 3.5svn)
+ * `g++ main.cpp -o bin/main`
+ */
+
+#include <iostream>
+
+int main() {
+    int sum = 0;
+    for (int n = 0; n < 1000; ++n) {
+        if (n % 3 == 0 || n % 5 == 0) {
+            sum += n;
+        }
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}

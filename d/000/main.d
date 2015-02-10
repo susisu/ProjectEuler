4e912bde046de7713316fd0af8b6e136b9831cbd
Susisu <susisu2413@yahoo.co.jp>
2015-02-11 00:13:39 +0900

    Add (D, 000)

---
+++ d/000/main.d
@@ -0,0 +1,14 @@
+/*
+ * ProjectEuler 000
+ * D / main.d
+ * copyright (c) 2015 Susisu
+ *
+ * DMD 2.066
+ * `dmd main.d -ofbin/main`
+ */
+
+import std.stdio;
+
+void main() {
+    writeln("Hello, world!");
+}

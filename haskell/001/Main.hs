0ffaef04effe1e10ec98a32911f9ef99cb12ba89
Susisu <susisu2413@yahoo.co.jp>
2015-02-11 13:27:07 +0900

    Update (Haskell, 001)

--- haskell/001/Main.hs
+++ haskell/001/Main.hs
@@ -10,4 +10,13 @@
 module Main where
 
 main :: IO ()
-main = print $ sum [n | n <- [1..999], n `mod` 3 == 0 || n `mod` 5 == 0]
+main = print $ calc 1 999 0
+
+calc :: Int -> Int -> Int -> Int
+calc n max sum
+    | n <= max =
+        if n `mod` 3 == 0 || n `mod` 5 == 0 then
+            calc (n + 1) max (sum + n)
+        else
+            calc (n + 1) max sum
+    | otherwise = sum

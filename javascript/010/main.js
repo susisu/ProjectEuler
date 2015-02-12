46a507b1c13c4b53b93b3522d6caa5e50310f748
Susisu <susisu2413@yahoo.co.jp>
2015-02-13 00:20:21 +0900

    Add (JavaScript, 010)

---
+++ javascript/010/main.js
@@ -0,0 +1,40 @@
+/*
+ * ProjectEuler 010
+ * JavaScript / main.js
+ * copyright (c) 2015 Susisu
+ *
+ * Node.js v0.12.0
+ * `node main.js`
+ */
+
+function main() {
+    console.log(sumPrimesBelow(2000000));
+}
+
+function sumPrimesBelow(n) {
+    if (n < 2) {
+        return 0;
+    }
+    else {
+        var isPrime = new Array(n);
+        isPrime[0] = isPrime[1] = false;
+        var i;
+        for (i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        var p = 2;
+        var sum = 0;
+        while (p < n) {
+            if (isPrime[p]) {
+                for (i = p * p; i < n; i += p) {
+                    isPrime[i] = false;
+                }
+                sum += p;
+            }
+            p++;
+        }
+        return sum;
+    }
+}
+
+main();

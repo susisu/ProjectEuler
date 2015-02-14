afcd19177e80c4db98051bbabfa315a2341def24
Susisu <susisu2413@yahoo.co.jp>
2015-02-14 17:44:30 +0900

    Update (Haskell, 010)

--- haskell/010/Main.hs
+++ haskell/010/Main.hs
@@ -4,7 +4,7 @@
     copyright (c) 2015 Susisu
 
     GHC 7.8.3
-    `ghc Main.hs -o bin/main`
+    `ghc Main.hs -o bin/main -O2`
 -}
 
 module Main where
@@ -12,29 +12,41 @@ module Main where
 import Control.Monad
 import Control.Monad.ST
 import Data.Array.ST
+import Data.Array.Unboxed
 import Data.List
 
 main :: IO ()
-main = print . foldl' (+) 0 $ primesBelow 2000000
+main = print $ calcTotal (sieveBelow 2000000)
 
-primesBelow :: Int -> [Int]
-primesBelow n = zipFilter (tableBelow n) [1 ..]
+calcTotal :: UArray Int Bool -> Int
+calcTotal table = let (start, end) = bounds table in loop start end 0
+    where
+        loop :: Int -> Int -> Int -> Int
+        loop n end total
+            | n > end   = total
+            | otherwise =
+                if table ! n then
+                    loop (n + 1) end (total + n)
+                else
+                    loop (n + 1) end total
 
-tableBelow :: Int -> [Bool]
-tableBelow n
-    | n < 2     = []
-    | otherwise = runST $ do
-        table <- newArray (1, n - 1) True :: ST s (STUArray s Int Bool)
+
+sieveBelow :: Int -> UArray Int Bool
+sieveBelow n
+    | n <= 0    = listArray (0, 0) [False]
+    | otherwise = runSTUArray $ do
+        table <- newArray (0, n - 1) True :: ST s (STUArray s Int Bool)
+        writeArray table 0 False
         writeArray table 1 False
-        forM_ [2 .. n - 1] $ \ p -> do
+        loop table 2 (floor . sqrt . fromIntegral $ n - 1)
+        return table
+    where
+        loop :: STUArray s Int Bool -> Int -> Int -> ST s ()
+        loop table p end = when (p <= end) $ do
             isPrime <- readArray table p
-            when isPrime
-                $ forM_ [p * p, p * p + p .. n - 1] $ \i ->
-                    writeArray table i False
-        getElems table
-
-zipFilter :: [Bool] -> [a] -> [a]
-zipFilter [] _ = []
-zipFilter _ [] = []
-zipFilter (True : cs) (x : xs)  = x : zipFilter cs xs
-zipFilter (False : cs) (_ : xs) = zipFilter cs xs
+            when isPrime $ fill table (p * p) p (n - 1)
+            loop table (p + 1) end
+        fill :: STUArray s Int Bool -> Int -> Int -> Int -> ST s ()
+        fill table i step end = when (i <= end) $ do
+            writeArray table i False
+            fill table (i + step) step end

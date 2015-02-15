cb081221b58310ff8a195c15b9cbc2e455f0e089
Susisu <susisu2413@yahoo.co.jp>
2015-02-15 17:52:41 +0900

    Add (C, 009)

---
+++ c/009/main.c
@@ -0,0 +1,22 @@
+/*
+ * ProjectEuler 009
+ * C / main.c
+ * copyright (c) 2015 Susisu
+ *
+ * GCC / Apple LLVM version 6.0 (clang-600.0.56) (based on LLVM 3.5svn)
+ * `gcc main.c -o bin/main`
+ */
+
+#include <stdio.h>
+
+int main() {
+    for (int a = 1; a <= 332; a++) {
+        for (int b = 2; b < 1000 - a - b; b++) {
+            if (2 * a * b - 2000 * (a + b) + 1000000 == 0) {
+                printf("%d\n", a * b * (1000 - a - b));
+                return 0;
+            }
+        }
+    }
+    return 0;
+}

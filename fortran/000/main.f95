cc835ff29a14f918917791ca92245be8b4bea2ce
Susisu <susisu2413@yahoo.co.jp>
2015-04-28 13:13:31 +0900

    Add Fortran

---
+++ fortran/000/main.f95
@@ -0,0 +1,11 @@
+! ProjectEuler 000
+! Fortran / main.f95
+! copyright (c) 2015 Susisu
+
+! GNU Fortran (Homebrew gcc 4.9.2_1) 4.9.2
+! `gfortran main.f95 -o bin/main`
+
+program main
+    implicit none
+    print *, "Hello, world!"
+end program main
